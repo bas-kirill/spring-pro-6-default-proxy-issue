c20ede7cc0dba91dfeff0fd5809d550cb3a9ef02
skywalker <baskirill.an@gmail.com>
2024-07-09 13:43:55 +0300

    fix(spring-pro-6): Spring AOP use CGLib by default starting from Spring 5th version
    Starting from Spring 5th maintainers changed proxy behaviour: CGLib used by default instead of previous strategy "JDK Proxy by default with fallback to CGLib"
    
    `org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration` at `spring-boot-autoconfigure` is a prove for it.
    
    Changes was caused by business requirements, so Spring Boot maintainers chose to change default behaviour

---
+++ src/test/kotlin/com/vmware/SpringPro6DefaultProxyStrategyIssueApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vmware
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringPro6DefaultProxyStrategyIssueApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
